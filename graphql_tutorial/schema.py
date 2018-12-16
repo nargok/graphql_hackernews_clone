1945f41d3e4bbddfd0a8fbe9d4537ea9983f7d77
nargok <nargok@gmail.com>
2018-12-16 20:13:53 +0900

    User mutaton

--- graphql_tutorial/schema.py
+++ graphql_tutorial/schema.py
@@ -1,11 +1,12 @@
 import graphene
 
 import links.schema
+from .users.schema import UserMutaion
 
 class Query(links.schema.Query, graphene.ObjectType):
   pass
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(UserMutaion, links.schema.Mutation, graphene.ObjectType):
   pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
