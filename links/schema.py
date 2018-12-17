fc18177884fea2efbc263d314205b23f1cb3bfb5
nargok <nargok@gmail.com>
2018-12-17 23:07:49 +0900

    Added posted_by field for Link Query and Mutation

--- links/schema.py
+++ links/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 
+from graphql_tutorial.users.schema import UserType
+
 from .models import Link
 
 class LinkType(DjangoObjectType):
@@ -17,19 +19,28 @@ class CreateLink(graphene.Mutation):
   id = graphene.Int()
   url = graphene.String()
   description = graphene.String()
+  posted_by = graphene.Field(UserType)
 
   class Arguments:
     url = graphene.String()
     description = graphene.String()
 
   def mutate(self, info, url, description):
-    link = Link(url=url, description=description)
+    user = info.context.user or None
+
+    link = Link(
+      url=url,
+      description=description,
+      posted_by=user,
+    )
     link.save()
 
+    # mutationにより返却される戻り値の設定
     return CreateLink(
       id=link.id,
       url=link.url,
       description=link.description,
+      posted_by=user,
     )
 
 class Mutation(graphene.ObjectType):
